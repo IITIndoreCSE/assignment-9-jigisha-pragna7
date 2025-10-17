729a953575ae2cd51c4d1c00286478ca33f7bd49
jigisha-pragna7 <mems250005051@iiti.ac.in>
2025-10-17 17:46:19 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+struct point{
+float x,y;
+};
+int main(){
+  point points[7];
+int count=0;
+for (int i=0; i<7; i++){
+cout<<" Point " << i+1 << "- x: ";
+cin>> points[i].x;
+cout<<" Point " << i+1 << "- y: ";
+cin>> points[i].y;
+}
+for (int i=0; i<7; i++){
+if (points[i].x >0 && points[i].y>0){
+count++;
+}
+}
+cout<<"Number of points in the first quadrant: "<<count<<endl;
+return 0;
+}
+
+
+
+
+
+ 
 
